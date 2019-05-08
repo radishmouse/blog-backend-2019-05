8f96b64007d1511b3824a5989fb90ee9865adbe1
Chris Aquino <chris@radishmouse.com>
2019-05-08 15:55:53 -0400

    adds tests for #from and #getById

--- models/User.js
+++ models/User.js
@@ -8,15 +8,30 @@ module.exports = class User {
     this.firstname = firstname;
     this.lastname = lastname;
   }
+  static from(obj) {
+    const u = new User();
+    Object.keys(obj).forEach(k => u[k] = obj[k]);
+    return u;
+  }
+  
+  static async getById(id) {
+    const result = await db.one(`
+select * from users where id=$1
+    `, [id]);
+    const user = User.from(result);
+
+    return user;
+  }
   
   async save() {
     const {id} = await db.one(`
 insert into users
-  (username, firstname, lastname, hash)
+  (username, hash, firstname, lastname)
 values
   ($1, $2, $3, $4)
 returning id
     `, [this.username, this.hash, this.firstname, this.lastname]);
     return id;
   }
-}
+
+};
--- test/test.js
+++ test/test.js
@@ -31,7 +31,42 @@ describe(`Users`, () => {
       expect(id).to.be.a('number');
     });
   });
-  // describe(`#getById`, () => {
-    // it(`should get one user`)
-  // });
+  describe(`#from`, () => {
+    it(`should create a User instance from an object`, () => {
+      const result = {
+        username: 'the_milla',
+        hash: '1234abcd',
+        firstname: 'milla',
+        lastname: 'aquino'
+      };
+      const u = User.from(result);
+      expect(u.username).to.equal(result.username);
+      expect(u.hash).to.equal(result.hash);
+      expect(u.firstname).to.equal(result.firstname);
+      expect(u.lastname).to.equal(result.lastname);      
+      
+    });
+  });
+  describe(`#getById`, () => {
+    it(`should get one user`, async () => {
+      const uname = 'radishmouse';
+      const phash = '12345';
+      const fname = 'chris';
+      const lname = 'aquino';
+      const u = new User(
+        uname, phash, fname, lname
+      );    
+      const id = await u.save();
+
+      const theUser = await User.getById(id);
+      expect(theUser).to.be.an.instanceOf(User);
+
+      expect(theUser.id).to.equal(theUser.id);
+      expect(theUser.username).to.equal(uname);
+      expect(theUser.hash).to.equal(phash);
+      expect(theUser.firstname).to.equal(fname);
+      expect(theUser.lastname).to.equal(lname);
+      
+    });
+  });
 });
