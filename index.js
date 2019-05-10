451a46210ce12066f592a5b58e8c9881919e8f07
chris aquino <chris@radishmouse.com>
2019-05-10 11:03:22 -0400

    serves static files from public folder

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(express.static('public'));
 
 // const userRouter = require('./routers/user');
 const postRouter = require('./routers/post');
 
 // app.use('/user', userRouter);
-app.use('/', postRouter);
+app.use('/api', postRouter);
 
 app.listen(port, () => {
   console.log(`backend listening on port ${port}`);
